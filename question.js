a767a26d2a1a9b9f63a579803da5397c60ea98b9
Nikunjvargotri <nv444480@gmail.com>
2025-03-19 01:36:48 +0530

    Add files via upload

--- question.js
+++ question.js
@@ -1,7 +1,28 @@
+function calculateElectricityBill(units) {
+    let totalBill = 0;
+    if (units <= 100) {
+        totalBill = units * 3;
+    } else if (units <= 200) {
+        totalBill = (100 * 3) + ((units - 100) * 5); 
+    } else if (units <= 300) {
+        totalBill = (100 * 3) + (100 * 5) + ((units - 200) * 7); 
+    } else {
+        totalBill = (100 * 3) + (100 * 5) + (100 * 7) + ((units - 300) * 10);
+    }
 
-   
-// 1) basic loop analisis
-for(let i=0; i<n; i++){
-console.log("hello")
+    
+    if (totalBill > 1000) {
+        totalBill += totalBill * 0.10; 
+    }
+
+    return totalBill;
 }
-// ans : time complexity of this question  is o(n)
+
+
+let unitsConsumed = 300; 
+let totalBill = calculateElectricityBill(unitsConsumed);
+
+console.log(`Total electricity bill for ${unitsConsumed} units: ₹${totalBill.toFixed(2)}`);
+
+  
+
