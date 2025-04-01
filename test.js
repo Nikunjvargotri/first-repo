806f2d26057447b0eec821bb1c4401fce49901f0
Nikunjvargotri <nv444480@gmail.com>
2025-04-01 09:35:23 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,40 @@
+// let arr=[5,6,3,2,1,8]
+// let max=0
+// for (let i=0; i<arr.length-1; i++){
+//     if (arr[i]>max){
+//         max=arr[i]
+       
+//     }
+// }
+// console.log(max)
+
+
+
+// let obj = [{name:"pawl",
+//         age:'22',
+//         marks:55 },
+//         {
+//             name:"manish",
+//             age:55,
+//             marks:75
+//         }
+
+// ]
+
+
+
+// let arr=[2,2,3,1,4,4]
+// let obj=[]
+// for (let i=0; i<arr.length; i++){
+
+// }
+
+
+
+
+
+
+
+
+
+
